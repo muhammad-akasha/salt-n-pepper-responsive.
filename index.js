a1f591e54a91556a0d0a38fe4be9be762b39a68c
Muhammad Akasha <153507162+muhammad-akasha@users.noreply.github.com>
2024-05-19 00:23:07 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000, // 3 seconds
+        autoplayHoverPause: true,
+        nav: false,
+        dots: false
+    });
+});
